83ca88628bfd94bf1b60d4152bb36e6d2217abed
Renaud Lemaire <rlemaire@cblue.be>
2024-02-15 08:19:11 +0100

    empty plugin

---
+++ lang/en/local_copygroups.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ *
+ * @date 14/02/2024
+ * @author Renaud Lemaire <rlemaire@cblue.be>
+ * @copyright 2024 CBlue SPRL {@link https://www.cblue.be}
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Copy groups';
