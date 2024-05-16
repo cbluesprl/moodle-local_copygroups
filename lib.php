364f99e6fa09cc78b64a9abc614bb9d89f906d83
Julien Bisiaux <jbisiaux@cblue.be>
2024-05-16 15:45:51 +0200

    CB-606808 : Fix capability required to display menu

--- lib.php
+++ lib.php
@@ -11,16 +11,14 @@
 /**
  * Display the edit custom information link in the course administration menu.
  *
- * @param settings_navigation $navigation The settings navigation object
- * @param stdClass $course The course
- * @param stdclass $context Course context
+ * @throws coding_exception
+ * @throws moodle_exception
  */
 function local_copygroups_extend_navigation_course($navigation, $course, $context) {
-    // Check that they can add an instance.
-
-    $url = new moodle_url('/local/copygroups/index.php', ['courseid' => $course->id]);
-    $settingsnode = navigation_node::create(get_string('course_import_groups_link', 'local_copygroups'), $url,
-        navigation_node::TYPE_SETTING, null, null, new pix_icon('i/settings', ''));
-    $navigation->add_node($settingsnode);
-
+    if(has_capability('moodle/course:managegroups', $context)) {
+        $url = new moodle_url('/local/copygroups/index.php', ['courseid' => $course->id]);
+        $settingsnode = navigation_node::create(get_string('course_import_groups_link', 'local_copygroups'), $url,
+            navigation_node::TYPE_SETTING, null, null, new pix_icon('i/settings', ''));
+        $navigation->add_node($settingsnode);
+    }
 }
