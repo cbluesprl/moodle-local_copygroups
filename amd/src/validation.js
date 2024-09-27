c5cb3f356f3d6c0c1f3140196a69aed8220966b6
Julien Bisiaux <jbisiaux@cblue.be>
2024-09-27 09:19:47 +0200

    CB-736431 : Update JS to new Modal Method (since Moodle 4.3)

--- amd/src/validation.js
+++ amd/src/validation.js
@@ -17,36 +17,41 @@
 // @copyright   2024 CBlue SPRL
 // @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 
-import ModalFactory from 'core/modal_factory';
+import ModalSaveCancel from 'core/modal_save_cancel';
 import ModalEvents from 'core/modal_events';
 import {get_string as getString} from 'core/str';
 
 export const init = (from) => {
     let popupAlreadyDisplayed = false;
     let form = document.getElementsByClassName("mform")[0];
-    form.addEventListener('submit', async(e) => {
-        if (!popupAlreadyDisplayed) {
-            e.preventDefault();
-            popupAlreadyDisplayed = true;
-            const modal = await ModalFactory.create({
-                type: ModalFactory.types.SAVE_CANCEL,
-                title: getString('modalcontentvalidationtitle', 'local_copygroups'),
-                large: false,
-                body: getString('modalcontentvalidation', 'local_copygroups'),
-            });
-            modal.getRoot().on(ModalEvents.save, () => {
-                document.getElementById("id_submitbutton").click();
-            });
-            modal.getRoot().on(ModalEvents.cancel, () => {
-                popupAlreadyDisplayed = false;
-            });
-            if (from === 'index') {
-                if (!document.getElementById('id_select_distinct_groups').checked) {
-                    modal.show();
+
+    if (form) {
+        form.addEventListener('submit', async(e) => {
+            if (!popupAlreadyDisplayed) {
+                e.preventDefault();
+                popupAlreadyDisplayed = true;
+
+                const modal = await ModalSaveCancel.create({
+                    title: getString('modalcontentvalidationtitle', 'local_copygroups'),
+                    body: getString('modalcontentvalidation', 'local_copygroups'),
+                });
+
+                modal.getRoot().on(ModalEvents.save, () => {
+                    document.getElementById("id_submitbutton").click();
+                });
+
+                modal.getRoot().on(ModalEvents.cancel, () => {
+                    popupAlreadyDisplayed = false;
+                });
+
+                if (from === 'index') {
+                    if (!document.getElementById('id_select_distinct_groups').checked) {
+                        await modal.show();
+                    }
+                } else if (from === 'groups_select') {
+                    await modal.show();
                 }
-            } else if (from === 'groups_select') {
-                modal.show();
             }
-        }
-    });
+        });
+    }
 };
