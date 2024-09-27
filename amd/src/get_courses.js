3d5cadc525ce03e75a526fdbb07828ecf871c543
Julien Bisiaux <jbisiaux@cblue.be>
2024-09-27 11:42:06 +0200

    CB-736431 : Implement AJAX to search courses

---
+++ amd/src/get_courses.js
@@ -0,0 +1,55 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @package     local_copygroups
+// @copyright   2024 CBlue SPRL
+// @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
+import Ajax from 'core/ajax';
+
+/**
+ * Load the list of groups where the users is enrolled
+ *
+ * @param {String} selector The selector of the auto complete element.
+ * @param {String} query The query string.
+ * @param {Function} callback A callback function receiving an array of results.
+ * @param {Function} failure A function to call in case of failure, receiving the error message.
+ */
+export async function transport(selector, query, callback, failure) {
+    const request = {
+        methodname: 'local_copygroups_get_courses',
+        args: {
+            query: query
+        }
+    };
+
+    try {
+        const response = await Ajax.call([request])[0];
+        callback(response);
+    } catch(e) {
+        failure(e);
+    }
+}
+
+/**
+ * Process the results for auto complete elements.
+ *
+ * @param {String} selector The selector of the auto complete element.
+ * @param {Array} results An array or results returned by {@see transport()}.
+ * @return {Array} New array of the selector options.
+ */
+export function processResults(selector, results) {
+    return results.map(result => ({value: result.id, label: result.shortname}));
+}
---
+++ classes/external/get_courses.php
@@ -0,0 +1,100 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_copygroups
+ * @copyright   2024 CBlue SPRL
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_copygroups\external;
+
+global $CFG;
+
+use external_api;
+use external_function_parameters;
+use external_multiple_structure;
+use external_single_structure;
+use external_value;
+
+require_once $CFG->dirroot . '/lib/externallib.php';
+
+class get_courses extends external_api
+{
+    /**
+     * @throws \coding_exception
+     * @throws \dml_exception
+     */
+    public static function get_courses(string $query)
+    {
+        global $DB, $USER;
+
+        $roles_can_import = get_config('local_copygroups', 'roles_can_import_groups');
+        if (!empty($roles_can_import)) {
+            [$insql, $inparams] = $DB->get_in_or_equal(explode(',', $roles_can_import), SQL_PARAMS_NAMED);
+
+            $sql = "SELECT DISTINCT c.id, c.shortname
+            FROM {role_assignments} ra
+            JOIN {context} ctx ON ra.contextid = ctx.id AND ctx.contextlevel = :context_course
+            JOIN {course} c ON ctx.instanceid = c.id
+            JOIN {user} u ON ra.userid = u.id
+            JOIN {role} r ON ra.roleid = r.id
+            JOIN {groups} g ON c.id = g.courseid
+            WHERE r.id $insql
+            AND u.id = :userid
+            AND " . $DB->sql_like('c.shortname', ':search', false) . "
+            LIMIT 10
+        ";
+            $params = [
+                'context_course' => CONTEXT_COURSE,
+                'roles_can_import' => get_config('local_copygroups', 'roles_can_import_groups'),
+                'userid' => $USER->id,
+                'search' => '%' . $DB->sql_like_escape($query) . '%'
+            ];
+
+            $req = $DB->get_records_sql($sql, $params + $inparams);
+
+            $courses = [];
+            foreach ($req as $c) {
+                $courses[] = [
+                    'id' => $c->id,
+                    'shortname' => $c->shortname
+                ];
+            }
+
+            return $courses;
+        } else {
+            return [];
+        }
+    }
+
+    public static function get_courses_parameters(): external_function_parameters
+    {
+        return new external_function_parameters([
+            'query' => new external_value(PARAM_TEXT, 'Course shortname', false)
+        ]);
+    }
+
+    public static function get_courses_returns(): external_multiple_structure
+    {
+        return new external_multiple_structure(
+            new external_single_structure([
+                'id' => new external_value(PARAM_INT, 'Course id'),
+                'shortname' => new external_value(PARAM_TEXT, 'Course shortname')
+            ])
+        );
+    }
+}
